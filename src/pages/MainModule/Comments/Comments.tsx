7099f163dabbd9b03516b660c19e7f2a5fdf40e6
Juan Calavera <juanconecti@gmail.com>
2022-08-05 18:40:07 -0300

    Create a Heroku file

---
+++ src/pages/MainModule/Comments/Comments.tsx
@@ -0,0 +1,10 @@
+import Header from "../../../components/Header/Header";
+import styles from "./Comments.module.scss";
+
+const Comments = () => {
+    return <div>
+        <Header title='Avaliações/comentários'/>
+    </div>;
+}
+
+export default Comments;
